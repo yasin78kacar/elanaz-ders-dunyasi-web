12087ea8f1b7cf890d48ffd6a6415a97a72450f8
Yasin Kacar <yasinkacar@gmail.com>
2026-06-29 23:14:29 +0200

    Add English Theme 1-2: 120 questions (Greetings & Colors/Numbers)

--- src/data.ts
+++ src/data.ts
@@ -1,13 +1,13 @@
 import turkce_50 from './data/turkce_50.json';
 import fen_50 from './data/fen_50.json';
 import hayat_50 from './data/hayat_50.json';
-import ingilizce_50 from './data/ingilizce_50.json';
+import english_tema_1_2 from './data/english/english_tema_1_2.json';
 import all_questions from './data/all_questions.json';
 
 export const dataMap: { [key: string]: any } = {
   turkce: turkce_50,
   fen: fen_50,
   hayat: hayat_50,
-  ingilizce: ingilizce_50,
+  ingilizce: english_tema_1_2,
   math: all_questions
 };
