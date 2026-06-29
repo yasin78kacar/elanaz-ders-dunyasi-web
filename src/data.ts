353c839474043b095185fd419a7cc34171c07fbc
Yasin Kacar <yasinkacar@gmail.com>
2026-06-29 23:25:32 +0200

    Add Turkish Theme 3: 75 questions (Writing & Sentence Formation)

--- src/data.ts
+++ src/data.ts
@@ -1,11 +1,11 @@
-import turkce_50 from './data/turkce_50.json';
+import turkce_tema3 from './data/turkce/turkce_tema3.json';
 import fen_50 from './data/fen_50.json';
 import hayat_50 from './data/hayat_50.json';
 import english_tema_1_2 from './data/english/english_tema_1_2.json';
 import all_questions from './data/all_questions.json';
 
 export const dataMap: { [key: string]: any } = {
-  turkce: turkce_50,
+  turkce: turkce_tema3,
   fen: fen_50,
   hayat: hayat_50,
   ingilizce: english_tema_1_2,
